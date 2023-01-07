d87e4a59a72158a5059638a77edb2d5df7effc57
roman <irkochnev@yandex.ru>
2023-01-07 11:36:50 +0100

    unused util functions removed

--- src/core/utils/index.js
+++ src/core/utils/index.js
@@ -1,43 +1,22 @@
 
+export const trim = (strings, ...values) => {
+  let output = '';
+  for (let i = 0; i < values.length; i++) {
+    output += strings[i] + values[i];
+  }
+  output += strings[values.length];
 
-export const util = {
+  // Split on newlines.
+  let lines = output.split(/(?:\r\n|\n|\r)/);
 
-  trim: (strings, ...values) => {
-    let output = '';
-    for (let i = 0; i < values.length; i++) {
-      output += strings[i] + values[i];
-    }
-    output += strings[values.length];
-
-    // Split on newlines.
-    let lines = output.split(/(?:\r\n|\n|\r)/);
-
-    // Rip out the leading whitespace.
-    return lines
-      .map(line => {
-        return line.replace(/^\s+/gm, '');
-      })
-      .join(' ')
-      .trim();
-  },
-
+  // Rip out the leading whitespace.
+  return lines
+    .map(line => {
+      return line.replace(/^\s+/gm, '');
+    })
+    .join(' ')
+    .trim();
 }
-
-export const debounce = (fn, delay = 0) => {
-  let timerId;
-
-  return function (...args) {
-    if (timerId) {
-      clearTimeout(timerId);
-    }
-
-    timerId = setTimeout(() => {
-      fn.apply(this, args);
-
-      timerId = null;
-    }, delay);
-  };
-};
 
 export const round = (number) => {
   return Math.ceil(number * 100) / 100;
